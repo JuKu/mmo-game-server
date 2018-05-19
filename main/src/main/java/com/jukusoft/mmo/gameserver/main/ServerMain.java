b530b3b89a8332c6e804892b7da2a52705eb8d51
JuKu <kuenzel.justin@t-online.de>
2018-05-19 17:47:08 +0200

    changed main class.

--- main/src/main/java/com/jukusoft/mmo/gameserver/main/ServerMain.java
+++ main/src/main/java/com/jukusoft/mmo/gameserver/main/ServerMain.java
@@ -3,7 +3,7 @@ package com.jukusoft.mmo.gameserver.main;
 public class ServerMain {
 
     public static void main (String[] args) {
-        //
+        System.out.println("======== Gameserver ========");
     }
 
 }
